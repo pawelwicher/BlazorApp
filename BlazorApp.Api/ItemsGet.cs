7f2be870c5160b08cba8898a7add706759378671
Paweł Wicher <pawel.wicher@pse.pl>
2021-12-19 20:37:06 +0100

    Blazor.Api

--- BlazorApp.Api/ItemsGet.cs
+++ BlazorApp.Api/ItemsGet.cs
@@ -1,22 +1,22 @@
-using System.Net;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorApp.Api
 {
     public static class ItemsGet
     {
-        [Function("ItemsGet")]
-        public static async Task<HttpResponseData> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "items")] HttpRequestData req,
-            FunctionContext executionContext)
+        [FunctionName(nameof(ItemsGet))]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "items")] HttpRequest req,
+            ILogger log)
         {
-            var response = req.CreateResponse(HttpStatusCode.OK);
+            var items = await Task.FromResult(Domain.Data.Items);
 
-            await response.WriteAsJsonAsync(BlazorApp.Domain.Data.Items);
-
-            return response;
+            return new OkObjectResult(items);
         }
     }
 }
